690202ee31a4cbc73585b5c36bd1d0f6bfbdea98
victor.martin <victor.martin@intraway.com>
2018-04-17 18:27:27 -0300

    restTemplate and object mapper for custom json mappings

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,14 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Wind;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
 
 @RestController
 public class DemoController {
+    private RestTemplate restTemplate = new RestTemplate();
+
 
     @GetMapping("/demo")
-    public String test() {
-        return "demo";
+    public Wind demo() throws IOException {
+        String uri ="https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (2460286)&format=json";
+
+        String inputString = restTemplate.getForObject(uri, ObjectNode.class)
+                .get("query")
+                .get("results")
+                .get("channel")
+                .get("wind")
+                .toString();
+
+        return new ObjectMapper().readValue(inputString, Wind.class);
     }
 
 }
